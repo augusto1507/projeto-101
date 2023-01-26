0a0e3b8aa2fa99fd8553dbbb592e0194fb2cd403
augusto1507 <99281591+augusto1507@users.noreply.github.com>
2023-01-26 14:51:32 -0300

    Add files via upload

---
+++ dado.py
@@ -0,0 +1,45 @@
+import random
+
+escolha="y"
+
+while escolha=="y":
+    no=random.randint(1,6)
+    if no==1:
+                print("---")
+                print("    ")
+                print(" 0 ")
+                print("   ")
+                print("---")
+    if no == 2: 
+            print("[-----]") 
+            print("[ 0   ]") 
+            print("[     ]") 
+            print("[   0 ]") 
+            print("[-----]") 
+    if no == 3: 
+            print("[-----]") 
+            print("[     ]") 
+            print("[0 0 0]") 
+            print("[     ]") 
+            print("[-----]") 
+    if no == 4: 
+            print("[-----]") 
+            print("[0   0]") 
+            print("[     ]") 
+            print("[0   0]") 
+            print("[-----]") 
+    if no == 5: 
+            print("[-----]") 
+            print("[0   0]") 
+            print("[  0  ]") 
+            print("[0   0]") 
+            print("[-----]") 
+    if no == 6: 
+            print("[-----]") 
+            print("[0 0 0]") 
+            print("[     ]") 
+            print("[0 0 0]") 
+            print("[-----]")        
+
+escolha=input(" se quiser continuar a jogar digite y")
+print("\n")
